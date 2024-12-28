825c63b17f80db2b80b992bdaff27efe78002437
trigardon10 <dennisjaeger10@yahoo.de>
2024-12-28 23:54:20 +0100

    feat: icon and export configs

--- src/Map1/Memory.gd
+++ src/Map1/Memory.gd
@@ -5,7 +5,7 @@ extends "res://src/unit.gd"
 func _ready() -> void:
 	unit_name = 'Memory Remnant'
 	unit_power = 3
-	unit_maxhealth = 10
+	unit_maxhealth = 8
 	unit_range = 4
 	unit_attack_range = 1
 	friendly = false
